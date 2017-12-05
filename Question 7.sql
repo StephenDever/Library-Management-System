01e00b4de343a238e1a14e4299fd011d78a87564
Stephen Harris <harrisbicycle@gmail.com>
2017-12-04 21:36:20 -0800

    Update Question 7.sql

--- Question 7.sql
+++ Question 7.sql
@@ -1,3 +1,5 @@
+/* For each book authored (or co-authored) by "Stephen King", retrieve the title and the number of
+copies owned by the library branch whose name is "Central" */
 SELECT Book.Title, Book_Authors.AuthorName, Book_Copies.No_Of_Copies, Library_Branch.BranchName
 FROM Book 
 JOIN Book_Copies
