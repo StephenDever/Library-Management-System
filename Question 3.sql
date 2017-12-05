e91c76a2d0e79affc0c9c9fed983cdd6abe10599
Stephen Harris <harrisbicycle@gmail.com>
2017-12-04 21:32:56 -0800

    Update Question 3.sql

--- Question 3.sql
+++ Question 3.sql
@@ -1,3 +1,4 @@
+/* Retrieve the names of all borrowers who do not have any books checked out. */
 SELECT * FROM Borrower LEFT JOIN Book_Loans
 ON Borrower.CardNo = Book_Loans.CardNo
 WHERE Book_Loans.CardNo IS NULL
