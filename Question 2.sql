005a5c59a87206c7e591ea6be54759ccdb8bdc8b
Stephen Harris <harrisbicycle@gmail.com>
2017-12-04 21:32:23 -0800

    Update Question 2.sql

--- Question 2.sql
+++ Question 2.sql
@@ -1,3 +1,4 @@
+/* How many copies of the book titled The Lost Tribe are owned by each library branch? */
 Select Library_Branch.BranchName, Book_Copies.No_Of_Copies 
 From Library_Branch
  Join Book_Copies On (Book_Copies.BranchID = Library_Branch.BranchID)
