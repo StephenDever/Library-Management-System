d1eb8226802f3050907c4257b26d973d80431f84
Stephen Harris <harrisbicycle@gmail.com>
2017-12-04 21:33:27 -0800

    Update Question 4.sql

--- Question 4.sql
+++ Question 4.sql
@@ -1,3 +1,5 @@
+/* For each book that is loaned out from the "Sharpstown" branch and whose DueDate is today,
+retrieve the book title, the borrower's name, and the borrower's address. */
 Select Book.Title, Borrower.Name, Borrower.[Address] from Library_Branch JOIN Book_Loans
 ON Library_Branch.BranchID = Book_Loans.BranchID 
 JOIN Book
