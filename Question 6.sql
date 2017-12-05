f43ad8ef3c6032a1ce2bc5c8370d3239798c554a
Stephen Harris <harrisbicycle@gmail.com>
2017-12-04 21:34:30 -0800

    Update Question 6.sql

--- Question 6.sql
+++ Question 6.sql
@@ -1,3 +1,5 @@
+/* Retrieve the names, addresses, and number of books checked out for all borrowers who have more
+than five books checked out. */
 SELECT Borrower.Name, Borrower.[Address], COUNT(Book_Loans.CardNo) AS No_Of_Books_Checked_Out
 FROM Borrower JOIN Book_Loans
 ON Borrower.CardNo = Book_Loans.CardNo
