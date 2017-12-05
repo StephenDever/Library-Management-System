72e86e1b459c5dc0b79c1accd2dfc39843fc2983
Stephen Harris <harrisbicycle@gmail.com>
2017-12-04 21:33:59 -0800

    Update Question 5.sql

--- Question 5.sql
+++ Question 5.sql
@@ -1,3 +1,5 @@
+/* For each library branch, retrieve the branch name and the total number of books loaned out from
+that branch. */
 SELECT Library_Branch.BranchName, COUNT(Book_Loans.BranchID) AS Total_Books_Out 
 FROM Book_Loans JOIN Library_Branch 
 ON Book_Loans.BranchID = Library_Branch.BranchID 
